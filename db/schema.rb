8ef5f29c7d3cb53fc3236f4b1647bcbb106105e5
Amy Kang <selene6023@gmail.com>
2015-01-13 00:42:43 -0800

    Added missing migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113052216) do
+ActiveRecord::Schema.define(version: 20150113083512) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 20150113052216) do
     t.boolean  "private",            default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
